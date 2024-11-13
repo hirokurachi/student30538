3539d4bcd1a906cbbe6b87f0db4ad1bc069acc58
hirokurachi <164104149+hirokurachi@users.noreply.github.com>
2024-11-13 12:28:52 -0600

    class work

--- before_lecture/shiny_11/apps_for_class/covid/app.py
+++ before_lecture/shiny_11/apps_for_class/covid/app.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 app_ui = ui.page_fluid(
     ui.input_select(id = 'state', label = 'Choose a state:',
     choices = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]),
+    ui.input_radio_buttons(id = "display", label = "Cases/Deaths", choices = ["cases", "deaths"]),
     ui.output_plot('ts'),
     ui.output_table("subsetted_data_table")
-
 )
 
 
@@ -30,10 +30,10 @@ def server(input, output, session):
     def ts():
         df = subsetted_data()
         fig, ax = plt.subplots(figsize=(3,6))
-        ax.plot(df['date'], df['cases'])
+        ax.plot(df['date'], df[input.display()])
         ax.tick_params(axis = 'x', rotation = 45)
         ax.set_xlabel('Date')
-        ax.set_ylabel('Cases')
+        ax.set_ylabel(input.display().capitalize())
         ax.set_title(f'COVID-19 cases in {input.state()}')
         ax.set_yticklabels(['{:,}'.format(int(x)) for x in ax.get_yticks()])
         return fig
